096caf5997f499408c1794a940e78079f2d93f14
Rodion Kuznetsov <kuznetsovrodion00@gmail.com>
2025-01-12 23:25:53 +0100

    Updated design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <main className="flex flex-col">
-      <div className="min-w-[580px] w-4/5 md:w-3/4 lg:w-3/5 mx-auto px-4 pb-1 bg-[#0D1E4C]/70 rounded-b-2xl shadow-2xl">
-        <div className="flex flex-col bg-[#0D1E4C]/50 mb-2 rounded-b-2xl shadow-xl">
+      <div className="min-w-[580px] w-4/5 md:w-3/4 lg:w-3/5 mx-auto mt-4 px-4 py-1 bg-[#0D1E4C]/70 rounded-2xl shadow-2xl">
+        <div className="flex flex-col bg-[#0D1E4C]/50 my-2 rounded-2xl shadow-xl">
           <h1 className="text-slate-50 text-3xl mx-auto w-fit mt-3 select-none">
             Choose Your Tool
           </h1>
 
-          <form className="flex-1 border-4 border-[#E5C9D7] divide-y-4 divide-[#E5C9D7] m-5 mb-3 rounded-xl grid grid-cols-1 grid-rows-2">
+          <form className="border-4 border-[#E5C9D7] divide-y-4 divide-[#E5C9D7] m-5 mb-3 rounded-xl grid grid-cols-1 grid-rows-2">
             <Link to="/balance-calculator">
               <div className="rounded-tl-lg rounded-tr-lg flex flex-col duration-500 hover:bg-[#E5C9D7]/15 text-2xl text-center text-white p-6">
                 Balance calculator
