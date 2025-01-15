a9c23f1c1737d03cd035fb1898a47a551cdcafab
Rodion Kuznetsov <kuznetsovrodion00@gmail.com>
2025-01-16 00:19:03 +0100

    Adjusted width

--- src/pages/DepositCalculator.jsx
+++ src/pages/DepositCalculator.jsx
@@ -43,7 +43,7 @@ const DepositCalculator = () => {
 
   return (
     <main className="flex flex-col transition-all duration-200 ease-in-out">
-      <div className="min-w-[580px] w-4/5 md:w-3/4 lg:w-3/5 mx-auto mt-4 px-4 py-1 bg-[#0D1E4C]/70 rounded-2xl shadow-2xl transform transition-all duration-200 ease-in-out">
+      <div className="min-w-[580px] w-4/5 md:w-3/4 lg:w-3/5 xl:w-2/5 2xl:w-1/3 mx-auto mt-4 px-4 py-1 bg-[#0D1E4C]/70 rounded-2xl shadow-2xl transform transition-all duration-200 ease-in-out">
         <div className="flex flex-col bg-[#0D1E4C]/50 my-2 rounded-2xl shadow-xl">
           <h1 className="text-slate-50 text-3xl mx-auto w-fit mt-3 select-none">
             Calculating Deposit
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <main className="flex flex-col">
-      <div className="min-w-[580px] w-4/5 md:w-3/4 lg:w-3/5 mx-auto mt-4 px-4 py-1 bg-[#0D1E4C]/70 rounded-2xl shadow-2xl">
+      <div className="min-w-[580px] w-4/5 md:w-3/4 lg:w-3/5 xl:w-2/5 2xl:w-1/3 mx-auto mt-4 px-4 py-1 bg-[#0D1E4C]/70 rounded-2xl shadow-2xl">
         <div className="flex flex-col bg-[#0D1E4C]/50 my-2 rounded-2xl shadow-xl">
           <h1 className="text-slate-50 text-3xl mx-auto w-fit mt-3 select-none">
             Choose Your Tool
